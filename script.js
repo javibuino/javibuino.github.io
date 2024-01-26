96d3dd40496a457f5a578ddec8f45d5d6a606bbb
javibuino <95667913+javibuino@users.noreply.github.com>
2024-01-26 14:48:51 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const dropdownButton = document.querySelector(".dropdown-button");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+dropdownButton.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("show");
+});
+
+document.addEventListener("click", (event) => {
+  if (!dropdownButton.contains(event.target)) {
+    dropdownMenu.classList.remove("show");
+  }
+});
