9fddec052d685c5e0721e49ada34dbadd4282922
javibuino <95667913+javibuino@users.noreply.github.com>
2024-01-26 14:32:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,29 @@ document.addEventListener("click", (event) => {
 });
 
 
-        return notes[note] || 0;
-    }
+        
+
+//Menu desplegable
+$(document).ready(function() {
+    $(".menubutton").click(function() { // trigger
+        $(this).next(".ul-list").slideToggle("fast"); // blendet beim Klick auf "dt" die nächste "dd" ein.
+        $(this).children("a").toggleClass("closed open"); // wechselt beim Klick auf "dt" die Klasse des enthaltenen a-Tags von "closed" zu "open".
+    });
+});
+
+
+
+//Banner Politica de privacidad
+// Función para ocultar el banner al hacer clic en "Entendido"
+function entendido() {
+  document.getElementById("avisoBanner").style.display = "none";
+  // Almacena la información en el almacenamiento local
+  localStorage.setItem("avisoBannerCerrado", "true");
+}
+
+// Mostrar el banner al cargar la página si no se ha cerrado anteriormente
+document.addEventListener("DOMContentLoaded", function() {
+  if (localStorage.getItem("avisoBannerCerrado") !== "true") {
+    document.getElementById("avisoBanner").style.display = "block";
+  }
 });
